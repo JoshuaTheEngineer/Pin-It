914f416f3428826abee2d837357fd0dabc0a71cf
Joshua Cadavez <13317525+JoshuaTheEngineer@users.noreply.github.com>
2022-11-29 13:27:08 -0500

    Create Contact screen (#4)
    * Add Contact list
    
    * Add contact list
    
    Co-authored-by: Joshua Cadavez <>

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,27 +1,81 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar} from 'react-native';
+
+const DATA = [
+  {
+    name: 'Joshua Cadavez',
+    phone: '1-911-911-9911',
+    location: {
+      name: 'Lakewood Ranch, FL',
+      latitude: 27.387918,
+      longitude: -82.43417
+    }
+  },
+  {
+    name: 'Jonathan Rameau',
+    phone: '1-222-333-4444',
+    location: {
+      name: 'Sarasota, FL',
+      latitude: 27.336433,
+      longitude: -82.531136
+    }
+  },
+  {
+    name: 'Calvin Tran',
+    phone: '1-555-678-9999',
+    location: {
+      name: 'Bradenton, FL',
+      latitude: 27.498928,
+      longitude: -82.574821
+    }
+  },
+]
+
+const Item = ({ name, phone, location }) => (
+  <View style={styles.item}>
+    <Text style={styles.name}>{name}</Text>
+    <Text style={styles.phone}>{phone}</Text>
+    <Text style={styles.location}>{location.name}</Text>
+  </View>
+);
 
 /**
  * The Contact Screen
  * @return {Screen}
  */
- export default function ContactScreen({ navigation }) {
+ export default function ContactScreen() {
+    const renderItem = ({ item }) => (
+      <Item name={item.name} phone={item.phone} location={item.location} />
+    )
     return (
-        <View style={styles.container}>
-            <Text>Hello World</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+          <FlatList 
+            data={DATA}
+            renderItem={renderItem}
+          />
+        </SafeAreaView>
       );
   }
   
   const styles = StyleSheet.create({
     container: {
-      ...StyleSheet.absoluteFillObject,
-      flex: 1, // the container will fill the whole screen.
-      justifyContent: 'flex-end',
-      alignItems: 'center',
+      flex: 1,
+      marginTop: StatusBar.currentHeight || 0,
     },
-    text: {
-      fontSize: 36,
+    item: {
+      backgroundColor: '#f9c2ff',
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+    },
+    name: {
+      fontSize: 32,
+    },
+    phone: {
+      fontSize: 16,
+    },
+    location: {
+      fontSize: 16,
     },
   });
   
